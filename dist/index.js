4162facaf4da2ed78864d1d53a2116f5a1370bb9
remal <remal@users.noreply.github.com>
2024-04-05 23:00:25 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -33418,28 +33418,37 @@ function newOctokitInstance(token) {
 const githubToken = core.getInput('githubToken', { required: true });
 core.setSecret(githubToken);
 const octokit = newOctokitInstance(githubToken);
+const defaultLimits = {
+    'core': 5_000,
+    'graphql': 5_000,
+    'search': 30,
+    'code_search': 10,
+    'integration_manifest': 5_000,
+    'code_scanning_upload': 500,
+    'actions_runner_registration': 10_000,
+    'scim': 15_000,
+    'dependency_snapshots': 100,
+    'source_import': null,
+};
 async function run() {
     try {
-        const rateLimit = await octokit.rateLimit.get({}).then(it => it.data);
+        const rateLimit = await octokit.rateLimit.get({})
+            .then(it => it.data);
         core.info("Rate limits: " + JSON.stringify(rateLimit, null, 2));
-        core.setOutput('coreLimit', rateLimit.resources.core?.limit ?? 5_000);
-        core.setOutput('coreUsed', rateLimit.resources.core?.used ?? 0);
-        core.setOutput('graphqlLimit', rateLimit.resources.graphql?.limit ?? 500_000);
-        core.setOutput('graphqlUsed', rateLimit.resources.graphql?.used ?? 0);
-        core.setOutput('searchLimit', rateLimit.resources.search?.limit ?? 10);
-        core.setOutput('searchUsed', rateLimit.resources.search?.used ?? 0);
-        core.setOutput('codeSearchLimit', rateLimit.resources.code_search?.limit ?? 10);
-        core.setOutput('codeSearchUsed', rateLimit.resources.code_search?.used ?? 0);
-        core.setOutput('integrationManifestLimit', rateLimit.resources.integration_manifest?.limit ?? 5_000);
-        core.setOutput('integrationManifestUsed', rateLimit.resources.integration_manifest?.used ?? 0);
-        core.setOutput('codeScanningUploadLimit', rateLimit.resources.code_scanning_upload?.limit ?? 500);
-        core.setOutput('codeScanningUploadUsed', rateLimit.resources.code_scanning_upload?.used ?? 0);
-        core.setOutput('actionsRunnerRegistrationLimit', rateLimit.resources.actions_runner_registration?.limit ?? 10_000);
-        core.setOutput('actionsRunnerRegistrationUsed', rateLimit.resources.actions_runner_registration?.used ?? 0);
-        core.setOutput('scimLimit', rateLimit.resources.scim?.limit ?? 15_000);
-        core.setOutput('scimUsed', rateLimit.resources.scim?.used ?? 0);
-        core.setOutput('dependencySnapshotsLimit', rateLimit.resources.dependency_snapshots?.limit ?? 100);
-        core.setOutput('dependencySnapshotsUsed', rateLimit.resources.dependency_snapshots?.used ?? 0);
+        for (const resource in defaultLimits) {
+            const defaultLimit = defaultLimits[resource];
+            if (defaultLimit == null) {
+                continue;
+            }
+            const limit = rateLimit.resources[resource]?.limit ?? defaultLimit;
+            const used = rateLimit.resources[resource]?.used ?? 0;
+            const usage = 100 * Math.floor(used / limit);
+            const outputName = resource.replaceAll(/_([a-z])/g, match => match[1].toUpperCase());
+            core.info(outputName);
+            core.setOutput(`${outputName}Usage`, usage);
+            core.setOutput(`${outputName}Limit`, limit);
+            core.setOutput(`${outputName}Used`, used);
+        }
     }
     catch (error) {
         core.setFailed(error instanceof Error ? error : `${error}`);
